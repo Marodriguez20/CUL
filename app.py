fb69f41d98702556d5704ab5a4ed8c556b4b7afa
Marodriguez20 <151986894+Marodriguez20@users.noreply.github.com>
2024-03-10 00:37:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask,request,jsonify
-import pymysql.cursors
-import pymysql
 
 app = Flask(__name__)
 
@@ -17,50 +15,6 @@ def get_personas():
  
     return jsonify(lista_personas)
 
-connection = pymysql.Connection(
-                                   host='localhost',
-                                   user= 'root',
-                                   port='3307',
-                                   password='',
-                                   db = 'bdpythonapi' ,
-                                   cursorclass=pymysql.cursors.DictCursor)
-
-     
-
-@app.route('/usuarios', methods=['GET'])
-def get_usuarios():
-     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM usuarios")
-        usuarios = cursor.fetchall()
-     return jsonify(usuarios)
-
-@app.route('/Usuario', methods=['POST'])
-def create_usuario():
-    data = request.get_json()
-    connection = ()
-    with connection:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO Usuario (nombre, email) VALUES (%s, %s)"
-            cursor.execute(sql, (data['nombre'],data['email']))
-        connection.commit()
-            
-    return jsonify({'message': 'creacion exitosa'}), 201
-
-@app.route('/usuarios/<int:usuario_id>', methods=['PUT'])
-def update_usuario(usuario_id):
-    nombre = request.json['nombre']
-    email = request.json['email']
-    with connection.cursor() as cursor:
-        cursor.execute("UPDATE usuarios SET nombre = %s, email = %s WHERE id = %s", (nombre, email, usuario_id))
-        connection.commit()
-    return jsonify({"message": "Usuario actualizado correctamente"})
-
-@app.route('/usuarios/<int:usuario_id>', methods=['DELETE'])
-def delete_usuario(usuario_id):
-    with connection.cursor() as cursor:
-        cursor.execute("DELETE FROM usuarios WHERE id = %s", (usuario_id,))
-        connection.commit()
-    return jsonify({"message": "Usuario eliminado correctamente"})
 
 if __name__ == '__main__':
     app.run(debug=True)
